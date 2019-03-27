3ef1ff9cb95e3e9ff493ecf94b383313eea7f0bb
Mohamed Fayez <mohamed.fayez@311-solutions.com>
2019-03-28 01:08:11 +0200

    removed unneeded routes from home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -23,19 +23,6 @@
     </style>
 @endsection
 @section('page-content')
-    <div>
-        @if (Route::has('login'))
-            <div class="top-right links">
-                @auth
-                    <a href="{{ url('/home') }}">Home</a>
-                @else
-                    <a href="{{ route('login') }}">Login</a>
-                    <a href="{{ route('register') }}">Register</a>
-                @endauth
-            </div>
-        @endif
-    </div>
-
     <div class="row align-items-center">
         <div class="col-sm">
             <form>

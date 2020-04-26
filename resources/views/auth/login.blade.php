75ddfdb18c23f447d449f5193e7a2d85e2b07e0a
Mohamed Fayez <mohamed.fayez@311-solutions.com>
2020-04-26 16:57:00 +0200

    Added new Auth layout, redesigned the login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,144 +1,71 @@
-@extends('templates.main')
+@extends('templates.auth')
 
 @section('page-styles')
     <style>
-        :root {
-            --input-padding-x: 1.5rem;
-            --input-padding-y: .75rem;
+        body,html {
+            height: 100%;
         }
-
-        body {
-            background: #007bff;
-            background: linear-gradient(to right, #0062E6, #33AEFF);
+        .bg-cover {
+            background-size: cover !important;
         }
-
-        .card-signin {
-            border: 0;
-            border-radius: 1rem;
-            box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
-        }
-
-        .card-signin .card-title {
-            margin-bottom: 2rem;
-            font-weight: 300;
-            font-size: 1.5rem;
-        }
-
-        .card-signin .card-body {
-            padding: 2rem;
-        }
-
-        .form-signin {
-            width: 100%;
-        }
-
-        .form-signin .btn {
-            font-size: 80%;
-            border-radius: 5rem;
-            letter-spacing: .1rem;
-            font-weight: bold;
-            padding: 1rem;
-            transition: all 0.2s;
-        }
-
-        .form-label-group {
-            position: relative;
-            margin-bottom: 1rem;
-        }
-
-        .form-label-group input {
-            height: auto;
-            border-radius: 2rem;
-        }
-
-        .form-label-group>input,
-        .form-label-group>label {
-            padding: var(--input-padding-y) var(--input-padding-x);
-        }
-
-        .form-label-group>label {
-            position: absolute;
-            top: 0;
-            left: 0;
-            display: block;
-            width: 100%;
-            margin-bottom: 0;
-            /* Override default `<label>` margin */
-            line-height: 1.5;
-            color: #495057;
-            border: 1px solid transparent;
-            border-radius: .25rem;
-            transition: all .1s ease-in-out;
-        }
-
-        .form-label-group input::-webkit-input-placeholder {
-            color: transparent;
-        }
-
-        .form-label-group input:-ms-input-placeholder {
-            color: transparent;
-        }
-
-        .form-label-group input::-ms-input-placeholder {
-            color: transparent;
-        }
-
-        .form-label-group input::-moz-placeholder {
-            color: transparent;
-        }
-
-        .form-label-group input::placeholder {
-            color: transparent;
-        }
-
-        .form-label-group input:not(:placeholder-shown) {
-            padding-top: calc(var(--input-padding-y) + var(--input-padding-y) * (2 / 3));
-            padding-bottom: calc(var(--input-padding-y) / 3);
-        }
-
-        .form-label-group input:not(:placeholder-shown)~label {
-            padding-top: calc(var(--input-padding-y) / 3);
-            padding-bottom: calc(var(--input-padding-y) / 3);
-            font-size: 12px;
-            color: #777;
-        }
-
-        .btn-google {
-            color: white;
-            background-color: #ea4335;
-        }
-
-        .btn-facebook {
-            color: white;
-            background-color: #3b5998;
+        .btn-text-shadow {
+            text-shadow: 1px 1px 2px #000000;
         }
     </style>
 @endsection
 @section('page-content')
-    <div class="row">
-        <div class="col-sm-9 col-md-7 col-lg-5 mx-auto">
-            <div class="card card-signin my-5">
-                <div class="card-body">
-                    <h5 class="card-title text-center">Bumblebee login</h5>
-                    <form class="form-signin" method="POST" action="{{url('login')}}">
-                        {{ csrf_field() }}
+    <div style="background: url({{asset('images/login-bg.jpg')}})"
+         class="bg-cover h-100">
+        <div class="row h-100 m-0">
+            <div class="col-lg-4 col-md-6 col-sm-8 mx-auto my-auto">
+                <div class="card card-login">
+                    <div class="card-header text-center">
+                        <a href="{{url('/')}}"><img class="img-fluid" src="{{asset('images/bumblebee_logo.png')}}"
+                             alt="Bumblebee Logo"></a>
+                    </div>
+                    <div class="card-body">
+                        <form class="form-signin" method="POST" action="{{url('login')}}">
+                            {{ csrf_field() }}
 
-                        <div class="form-label-group">
-                            <input id="email" class="form-control" name="email" placeholder="Email" required autofocus>
-                            <label for="email">Email</label>
-                        </div>
+                            <div class="bmd-form-group my-4">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">
+                                            <i class="material-icons">email</i>
+                                        </span>
+                                    </div>
+                                    <input id="email" class="form-control" name="email" placeholder="Email" required autofocus>
+                                </div>
+                            </div>
 
-                        <div class="form-label-group">
-                            <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
-                            <label for="password">Password</label>
-                        </div>
+                            <div class="bmd-form-group my-4">
+                                <div class="input-group">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">
+                                            <i class="material-icons">lock_outline</i>
+                                        </span>
+                                    </div>
+                                    <input type="password" id="password" name="password" class="form-control" placeholder="Password" required>
+                                </div>
+                            </div>
 
-                        <div class="custom-control custom-checkbox mb-3">
-                            <input type="checkbox" class="custom-control-input" id="remember-me" name="remember">
-                            <label class="custom-control-label" for="remember-me">Remember me</label>
-                        </div>
-                        <button class="btn btn-lg btn-primary btn-block text-uppercase" type="submit">Login</button>
-                    </form>
+                            <div class="col-md-9 my-4">
+                                <div class="form-check">
+                                    <label class="form-check-label" for="remember-me">
+                                        <input type="checkbox" class="form-check-input" id="remember-me" name="remember">
+                                        Remember me
+                                        <span class="form-check-sign">
+                                            <span class="check"></span>
+                                        </span>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="d-flex justify-content-center align-content-center">
+                                <button class="btn btn-lg btn-primary btn-round text-uppercase px-5 bg-warning btn-text-shadow"
+                                    type="submit">Login</button>
+                            </div>
+                        </form>
+                    </div>
                 </div>
             </div>
         </div>
---
+++ resources/views/templates/auth.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Bumblebee</title>
+
+    <!-- Styles -->
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{asset('css/fontawesome/all.css')}}" rel="stylesheet">
+    <link href="{{asset('css/main.css')}}" rel="stylesheet">
+    <link href="{{asset('css/material-dashboard.min.css')}}" rel="stylesheet">
+    @yield('page-styles')
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+</head>
+
+<body>
+
+<!-- Navigation -->
+
+<!-- Page Content -->
+<div class="container-fluid h-100 p-0">
+    @include('partials.flash')
+
+    @yield('page-content')
+</div>
+
+<!-- Scripts -->
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
+@yield('page-scripts')
+</body>
+</html>

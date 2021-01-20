840a367bffe638f15f0196d84fef3d799fcb5716
Mohamed Fayez <mohamed.fayez@311-solutions.com>
2021-01-20 02:56:19 +0200

    Added backend data retrieval for the DoOrder admin dashboard top cards

--- app/Http/Controllers/doorder/DashboardController.php
+++ app/Http/Controllers/doorder/DashboardController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers\doorder;
 
+use App\DriverProfile;
 use App\Order;
+use App\Retailer;
 use App\User;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -20,7 +22,21 @@ class DashboardController extends Controller
             }
             return view('admin.doorder.retailers.dashboard',compact('drivers_arr'));
         } else {
-            $admin_data = $this->adminDashboardData();
+            $from_date = null;
+            $to_date = null;
+            $filter_from_date = $request->get('from_date');
+            $filter_to_date = $request->get('to_date');
+            if($filter_from_date!=null && $filter_to_date!=null){
+                $filter_from_date = new Carbon($filter_from_date);
+                $filter_to_date = new Carbon($filter_to_date);
+                $from_date = $filter_from_date->startOfDay()->toDateTimeString();
+                $to_date = $filter_to_date->endOfDay()->toDateTimeString();
+            }
+            $admin_data = $this->adminDashboardData($from_date,$to_date);
+            $all_orders_count = $admin_data['all_orders_count'];
+            $delivered_orders_count = $admin_data['delivered_orders_count'];
+            $retailers_count = $admin_data['retailers_count'];
+            $deliverers_count = $admin_data['deliverers_count'];
             $drivers_arr = $admin_data['drivers_arr'];
             return view('admin.doorder.dashboard',compact('drivers_arr'));
         }
@@ -75,7 +91,30 @@ class DashboardController extends Controller
         return ['drivers_arr'=>$drivers_arr];
     }
 
-    private function adminDashboardData(){
+    private function adminDashboardData($from_date,$to_date){
+        $custom_date = true;
+        $current_date = Carbon::now();
+        if($from_date==null && $to_date==null) {
+            $custom_date = false;
+            $from_date = $current_date->startOfDay()->toDateTimeString();
+            $to_date = $current_date->endOfDay()->toDateTimeString();
+        }
+        $all_orders = Order::whereBetween('created_at',[$from_date,$to_date])->get();
+        $all_orders_count = count($all_orders);
+        $delivered_orders_count = 0;
+        foreach($all_orders as $order){
+            if($order->status == 'delivered'){
+                $delivered_orders_count++;
+            }
+        }
+        if($custom_date == false) {
+            $from_date = $current_date->startOfMonth()->startOfDay()->toDateTimeString();
+            $to_date = $current_date->endOfMonth()->endOfDay()->toDateTimeString();
+        }
+        $registered_retailers = Retailer::whereBetween('created_at',[$from_date,$to_date])->get();
+        $registered_deliverers = DriverProfile::whereBetween('created_at',[$from_date,$to_date])->get();
+        $retailers_count = count($registered_retailers);
+        $deliverers_count = count($registered_deliverers);
         $drivers = User::with('driver_profile')->where('user_role','=','driver')->get();
         $drivers_arr = [];
         foreach($drivers as $driver){
@@ -96,6 +135,8 @@ class DashboardController extends Controller
             }
         }
         $drivers_arr = json_encode($drivers_arr);
-        return ['drivers_arr'=>$drivers_arr];
+        return ['drivers_arr'=>$drivers_arr, 'all_orders_count'=>$all_orders_count,
+            'delivered_orders_count'=>$delivered_orders_count, 'retailers_count'=>$retailers_count,
+            'deliverers_count'=>$deliverers_count];
     }
 }

f5e64804fd99f78cab20be55f5db48e3054f9390
Hisham Magdy <hisham.magdy@311-solutions.com>
2021-03-04 18:39:36 +0200

    garden help socket file

---
+++ socketIO_gardenhelp.js
@@ -0,0 +1,36 @@
+var app = require('express')();
+
+var https = require('https');
+var fs = require('fs');
+var options = {
+    key: fs.readFileSync('/etc/ssl/private/iot.bumblebeeai.io.key'),
+    cert: fs.readFileSync('/etc/ssl/certs/iot_bumblebeeai_io.crt')
+};
+
+var server = https.createServer(options, app);
+
+var io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+    }
+});
+var redis = require('redis');
+
+server.listen(8890);
+console.log('Server is running.....');
+io.on('connection', function (socket) {
+
+    console.log("new client connected");
+    var redisClient = redis.createClient();
+    redisClient.subscribe('doorder-channel');
+
+    redisClient.on("message", function(channel, data) {
+        let decodedData = JSON.parse(data)
+        socket.emit(channel + ':' + decodedData.event, data);
+        console.log(data)
+    });
+
+    socket.on('disconnect', function() {
+        redisClient.quit();
+    });
+});

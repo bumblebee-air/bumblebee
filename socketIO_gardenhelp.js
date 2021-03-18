31ef56a395e68a340fc3770faf84ec52ebf514af
Hisham Magdy <hisham.magdy@311-solutions.com>
2021-03-18 11:48:59 +0200

    adding garden help socket event

--- socketIO_gardenhelp.js
+++ socketIO_gardenhelp.js
@@ -22,7 +22,7 @@ io.on('connection', function (socket) {
 
     console.log("new client connected");
     var redisClient = redis.createClient();
-    redisClient.subscribe('doorder-channel');
+    redisClient.subscribe('garden-help-channel');
 
     redisClient.on("message", function(channel, data) {
         let decodedData = JSON.parse(data)

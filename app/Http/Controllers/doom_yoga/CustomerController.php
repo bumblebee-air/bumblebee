6fc05eda785e8fa417ba34a09c668816d3210eb6
Hisham Magdy <hisham.magdy@311-solutions.com>
2021-04-30 01:58:27 +0200

    Video lib controller code

--- app/Http/Controllers/doom_yoga/CustomerController.php
+++ app/Http/Controllers/doom_yoga/CustomerController.php
@@ -11,11 +11,7 @@ use Illuminate\Http\Request;
 class CustomerController extends Controller
 {
     public function getCustomerRegistrationForm(Request $request) {
-//        return view('doom_yoga.customers.registration');
-        return view('doom_yoga.customers.registration_card_details', [
-            'customer_id' => '',
-            'price_id' => '',
-        ]);
+        return view('doom_yoga.customers.registration');
     }
     
     public function postCustomerRegistrationForm(Request $request) {
@@ -111,5 +107,59 @@ class CustomerController extends Controller
     
     public function getCustomerAccount(){
         return view('doom_yoga.customers.account_home');        
+    }
+
+    public function getVideoLibrary()
+    {
+        $videoData1 = new VideoData("images/wheat-field.mp4", "images/doom-yoga/doom-yoga-logo.png", "Mental Training", "04 Min 20 Sec");
+        // dd($videoData1);
+        $videoData2 = new VideoData("images/wheat-field.mp4", "images/doom-yoga/doom-yoga-logo.png", "For Losing Weight", "12 Min 20 Sec");
+
+        $videos = array(
+            $videoData1,
+            $videoData2,
+            $videoData1,
+            $videoData2,
+            $videoData1,
+            $videoData2,
+            $videoData1,
+            $videoData2
+        );
+
+        $categoryData1 = new CategoryData('Category 1', $videos);
+        $categoryData2 = new CategoryData('Category 2', $videos);
+        $categories = array(
+            $categoryData1,
+            $categoryData2
+        );
+        return view('doom_yoga.customers.video_library', [
+            'categories' => json_encode($categories)
+        ]);
+    }
+}
+
+class CategoryData
+{
+
+    public $title, $videos;
+
+    function __construct($title, $videos)
+    {
+        $this->title = $title;
+        $this->videos = $videos;
+    }
+}
+
+class VideoData
+{
+
+    public $videoUrl, $posterImageUrl, $title, $duration;
+
+    function __construct($videoUrl, $posterImageUrl, $title, $duration)
+    {
+        $this->videoUrl = $videoUrl;
+        $this->posterImageUrl = $posterImageUrl;
+        $this->title = $title;
+        $this->duration = $duration;
     }
 }

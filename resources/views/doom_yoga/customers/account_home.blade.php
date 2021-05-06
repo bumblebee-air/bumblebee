ffb4f3351d19f1aa54bfc8a0412e4f69dc8be195
Sara Yassen <sarayassen.118@gmail.com>
2021-05-06 02:04:13 +0200

    doom yoga: added meditation library page

--- resources/views/doom_yoga/customers/account_home.blade.php
+++ resources/views/doom_yoga/customers/account_home.blade.php
@@ -13,10 +13,11 @@ body {
 #myVideo {
 	 object-fit: cover;
   width: 100vw;
-  height: 100vh;
-  position: fixed;
-  top: 0;
-  left: 0;
+  height: 100vh; position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
 }
 </style>
 @endsection @section('content')

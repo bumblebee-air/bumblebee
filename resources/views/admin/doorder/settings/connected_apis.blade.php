2ecec290b04b8192210873f2d055e33cb50f3ec1
Sara Yassen <sarayassen.118@gmail.com>
2021-08-18 10:59:09 +0200

    doorder: add deliverer payout field in stripe section in settings page

--- resources/views/admin/doorder/settings/connected_apis.blade.php
+++ resources/views/admin/doorder/settings/connected_apis.blade.php
@@ -19,7 +19,7 @@
 					<div class="col-sm-6">
 						<div class="form-group bmd-form-group ">
 							<label>Retailer automatic charging </label>
-							<div class="togglebutton" id="toggleButtonConnectedApi"
+							<div class="togglebutton toggleButtonConnectedApi"
 								style="display: inline-block;">
 								<label> <input type="checkbox" id="retailerAutomaticCharging"
 									value="1" name="retailerAutomaticCharging"
@@ -36,13 +36,50 @@
 					<div class="col-sm-6">
 						<div class=" ">
 							<label class="control-label" for="dayOfMonth">Day of month </label><select
-								class="form-control "
-								data-style="select-with-transition" name="dayOfMonth"
-								id="dayOfMonth"> 
-								<option value=""> Select day </option>
-								@for ($i = 1; $i <= 31; $i++)
+								class="form-control " data-style="select-with-transition"
+								name="dayOfMonth" id="dayOfMonth">
+								<option value="">Select day</option> @for ($i = 1; $i <= 31;
+								$i++)
 								<option value="{{$i}}">{{$i}}</option> @endfor
 
+
+							</select>
+						</div>
+					</div>
+				</div>
+
+				<div class="row mt-2">
+
+					<div class="col-sm-6">
+						<div class="form-group bmd-form-group ">
+							<label>Deliverer automatic payout </label>
+							<div class="togglebutton toggleButtonConnectedApi"
+								style="display: inline-block;">
+								<label> <input type="checkbox" id="delivererPayout" value="1"
+									name="delivererPayout" onclick="changeToggleDelivererPayout()">
+									<span class="toggle"></span>
+								</label>
+							</div>
+						</div>
+
+					</div>
+				</div>
+
+				<div class="row" id="dayOfWeekDiv" style="display: none">
+
+					<div class="col-sm-6">
+						<div class=" ">
+							<label class="control-label" for="weekday">Weekday </label><select
+								class="form-control " data-style="select-with-transition"
+								name="weekday" id="weekday">
+								<option value="">Select day</option>
+								<option value="Monday">Monday</option>
+								<option value="Tuesday">Tuesday</option>
+								<option value="Wednesday">Wednesday</option>
+								<option value="Thursday">Thursday</option>
+								<option value="Friday">Friday</option>
+								<option value="Saturday">Saturday</option>
+								<option value="Sunday">Sunday</option>
 
 							</select>
 						</div>

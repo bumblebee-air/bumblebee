925eb5146b96102aee0bf8e76dc37d381b217e59
Sara Yassen <sarayassen.118@gmail.com>
2021-11-29 02:21:15 +0200

    doorder (new layout): edited drivers request, retailers requests list and retailers list pages

--- resources/views/admin/doorder/drivers/requests.blade.php
+++ resources/views/admin/doorder/drivers/requests.blade.php
@@ -1,8 +1,10 @@
-@extends('templates.dashboard')
+@extends('templates.doorder_dashboard')
 
 @section('page-styles')
     <style>
-        
+        table td{
+            text-align: left !important;
+        }
         
     </style>
 @endsection
@@ -14,46 +16,28 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-12">
+                    	<div class="card">
+    						<div class="card-header card-header-icon card-header-rose row">
+    							<div class="col-12">
+    
+    								<h4 class="card-title my-4">Drivers Requests</h4>
+    							</div>
+    							
+    						</div>
+						</div>
                         <div class="card">
-                            <div class="card-header card-header-icon card-header-rose row">
-                               <div class="col-12 col-lg-6 col-md-6">
-                                    <div class="card-icon">
-                                        <img class="page_icon" src="{{asset('images/doorder_icons/drivers_requests.png')}}">
-                                    </div>
-                                    <h4 class="card-title ">Drivers Requests</h4>
-                                </div>
-                                <div class="col-12 col-lg-6 col-md-6 mt-md-3">
-                                    <div class="row justify-content-end">
-                                        <div class="status">
-                                            <div class="status_item">
-                                                <img class="status_icon" src="{{asset('images/doorder_icons/order_status_ready.png')}}" alt="Request received">
-                                                Request received
-                                            </div>
-                                            <div class="status_item">
-                                                <img class="status_icon" src="{{asset('images/doorder_icons/order_status_on_route_pickup.png')}}" alt="Missing Data">
-                                                Missing Data
-                                            </div>
-                                            <div class="status_item">
-                                                <img class="status_icon" src="{{asset('images/doorder_icons/order_status_delivered.png')}}" alt="Request completed">
-                                                Request completed
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            
                             <div class="card-body">
-                                <div class="float-right">
-                                  
-                                </div>
+                                
                                 <div class="table-responsive">
-                                    <table class="table" id="driversTable"
+                                    <table class="table table-no-bordered table-hover doorderTable ordersListTable" id="driversTable"
 									cellspacing="0" width="100%" >
                                         <thead>
                                             <tr>
+                                                <th>Number</th>
                                                 <th>Date/Time</th>
                                                 <th>Location</th>
                                                 <th>Deliverer Name</th>
-                                                <th>Application No.</th>
                                                 <th>Status</th>
                                                 <th>Stage</th>
                                                 <th>Address</th>
@@ -62,26 +46,36 @@
 
                                         <tbody>
                                             <tr v-for="driver_request in drivers_requests.data" v-if="drivers_requests.data.length > 0" class="order-row" @click="openRequest(driver_request.id)">
-                                                <td>@{{ parseDateTime(driver_request.created_at) }}</td>
+                                                <td>
+                                                    #@{{ driver_request.id}}
+                                                </td>
+                                                 <td class="text-left orderDateTimeTd">@{{ parseDateTime(driver_request.created_at) }}</td>
                                                 <td>
                                                     @{{ JSON.parse(driver_request.work_location).name}}
                                                 </td>
                                                 <td>
                                                     @{{ driver_request.first_name}} @{{ driver_request.last_name  }}
                                                 </td>
+                                               
                                                 <td>
-                                                    @{{ driver_request.id}}
-                                                </td>
-                                                <td>
-                                                    <img class="status_icon" src="{{asset('images/doorder_icons/order_status_ready.png')}}" v-if="driver_request.status == 'received'" alt="Request received">
-                                                    <img class="status_icon" src="{{asset('images/doorder_icons/order_status_on_route_pickup.png')}}" v-if="driver_request.status == 'missing'" alt="Missing Data">
-                                                    <img class="status_icon" src="{{asset('images/doorder_icons/order_status_delivered.png')}}" v-if="driver_request.status == 'completed'" alt="Request completed">
+                                                	<span
+															v-if="driver_request.status == 'received'"
+															class="orderStatusSpan receivedStatus">Request recieved</span>
+                                                	<span
+															v-if="driver_request.status == 'stripe_form_sent'"
+															class="orderStatusSpan stripeFormSentStatus">Stripe form sent</span>
+                                                	<span
+															v-if="driver_request.status == 'missing'"
+															class="orderStatusSpan missingStatus">Missing data</span>
+                                                	<span
+															v-if="driver_request.status == 'completed'"
+															class="orderStatusSpan completedStatus">Completed</span>
                                                 </td>
                                                 @php($i = 33.34)
                                                 <td>
                                                     <div class="progress">
                                                         <div class="progress-bar" role="progressbar" style="width: {{1 * $i}}%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" v-if="driver_request.status == 'received'"></div>
-                                                        <div class="progress-bar" role="progressbar" style="width: {{2 * $i}}%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" v-if="driver_request.status == 'missing'"></div>
+                                                        <div class="progress-bar" role="progressbar" style="width: {{2 * $i}}%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" v-if="driver_request.status == 'missing' || driver_request.status =='stripe_form_sent'"></div>
                                                         <div class="progress-bar" role="progressbar" style="width: {{3 * $i}}%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" v-if="driver_request.status == 'completed'"></div>
                                                     </div>
                                                 </td>
@@ -124,14 +118,19 @@ $(document).ready(function() {
     	
           fixedColumns: true,
           "lengthChange": false,
-          "searching": false,
+          "searching": true,
   		  "info": false,
   		  "ordering": false,
   		  "paging": false,
+  		  
+           "language": {  
+            	search: '',
+        		"searchPlaceholder": "Search ",
+           },
     	 columnDefs: [
                 {
                     render: function (data, type, full, meta) {
-                    	return '<span data-toggle="tooltip" data-placement="top" title="'+data+'">'+data+'</span>';
+                    	return '<span class="tableTextSpan" data-toggle="tooltip" data-placement="top" title="'+data+'">'+data+'</span>';
                     },
                     targets: -1
                 }
@@ -177,16 +176,10 @@ $(document).ready(function() {
                     window.location.href = "{{url('doorder/drivers/requests/')}}/"+request_id;
                 },
                 parseDateTime(date) {
-                    console.log(date);
+                   // console.log(date);
                     let dateTime = '';
-                    //let parseDate = new Date(date);
                     let date_moment = new moment(date);
-                    /*dateTime += parseDate.getDate() + '/';
-                    dateTime += parseDate.getMonth()+1 + '/';
-                    dateTime += parseDate.getFullYear() + ' ';
-                    dateTime += parseDate.getHours() + ':';
-                    dateTime += parseDate.getMinutes();*/
-                    dateTime = date_moment.format('DD-MM-YYYY HH:mm');
+                    dateTime = date_moment.format('DD MMM YY HH:mm');
                     return dateTime;
                 }
             }
--- resources/views/admin/doorder/retailers/requests.blade.php
+++ resources/views/admin/doorder/retailers/requests.blade.php
@@ -1,72 +1,65 @@
-@extends('templates.dashboard')
+@extends('templates.doorder_dashboard')
 
 @section('page-styles')
     <style>
       
+        table td{
+            text-align: left !important;
+        }
     </style>
 @endsection
 
-@section('title', 'DoOrder | Orders')
+@section('title', 'DoOrder | Retailers Requests')
 @section('page-content')
     <div class="content">
         <div class="container-fluid">
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-12">
+                    	<div class="card">
+    						<div class="card-header card-header-icon card-header-rose row">
+    							<div class="col-12">
+    
+    								<h4 class="card-title my-4">Retailers Requests</h4>
+    							</div>
+    							
+    						</div>
+						</div>
                         <div class="card">
-                            <div class="card-header card-header-icon card-header-rose row">
-                               <div class="col-12 col-lg-6 col-md-6">
-                                    <div class="card-icon">
-                                        <img class="page_icon" src="{{asset('images/doorder_icons/drivers_requests.png')}}">
-                                    </div>
-                                    <h4 class="card-title ">Retailers Requests</h4>
-                                </div>
-                                <div class="col-12 col-lg-6 col-md-6 mt-md-3">
-                                    <div class="row justify-content-end">
-                                        <div class="status">
-                                            <div class="status_item">
-                                                <img class="status_icon" src="{{asset('images/doorder_icons/order_status_ready.png')}}" alt="Request received">
-                                                Request received
-                                            </div>
-                                            <div class="status_item">
-                                                <img class="status_icon" src="{{asset('images/doorder_icons/order_status_on_route_pickup.png')}}" alt="Missing Data">
-                                                Missing Data
-                                            </div>
-                                            <div class="status_item">
-                                                <img class="status_icon" src="{{asset('images/doorder_icons/order_status_delivered.png')}}" alt="Request completed">
-                                                Request completed
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                           
                             <div class="card-body">
-                                <div class="float-right">
-                                    
-                                </div>
                                 <div class="table-responsive">
-                                    <table class="table" id="retailersTable"
+                                    <table class="table table-no-bordered table-hover doorderTable ordersListTable" id="retailersTable"
 									cellspacing="0" width="100%" >
-                                        <thead> <tr>
-                                        <th>Date/Time</th>
-                                        <th>Business Type</th>
-                                        <th>Retailer Name</th>
-                                        <th>Application No.</th>
-                                        <th>Status</th>
-                                        <th>Stage</th>
-                                        <th>Locations No.</th>
-                                       </tr> </thead>
+                                        <thead> 
+                                            <tr>
+                                            <th>Number</th>
+                                            <th>Date/Time</th>
+                                            <th>Business Type</th>
+                                            <th>Retailer Name</th>
+                                            <th>Status</th>
+                                            <th>Stage</th>
+                                            <th class="text-center">Locations No.</th>
+                                           </tr> 
+                                       </thead>
 
                                         <tbody>
                                             <tr v-for="request in retailers_requests.data" v-if="retailers_requests.data.length > 0" class="order-row" @click="openRequest(request.id)">
-                                                <td>@{{ parseDateTime(request.created_at) }}</td>
+                                                <td>#@{{ request.id}}</td>
+                                                <td  class="text-left orderDateTimeTd">@{{ parseDateTime(request.created_at) }}</td>
                                                 <td>@{{ request.business_type}}</td>
                                                 <td>@{{ request.name}}</td>
-                                                <td>@{{ request.id}}</td>
                                                 <td>
-                                                    <img class="status_icon" src="{{asset('images/doorder_icons/order_status_ready.png')}}" v-if="request.status == 'received'" alt="Request received">
-                                                    <img class="status_icon" src="{{asset('images/doorder_icons/order_status_on_route_pickup.png')}}" v-if="request.status == 'missing'" alt="Missing Data">
-                                                    <img class="status_icon" src="{{asset('images/doorder_icons/order_status_delivered.png')}}" v-if="request.status == 'completed'" alt="Request completed">
+                                                	
+                                                	<span
+															v-if="request.status == 'received'"
+															class="orderStatusSpan receivedStatus">Request recieved</span>
+                                                	<span
+															v-if="request.status == 'missing'"
+															class="orderStatusSpan missingStatus">Missing data</span>
+                                                	<span
+															v-if="request.status == 'completed'"
+															class="orderStatusSpan completedStatus">Completed</span>
                                                 </td>
                                                 @php($i = 33.34)
                                                 <td>
@@ -76,7 +69,7 @@
                                                         <div class="progress-bar" role="progressbar" style="width: {{3 * $i}}%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" v-if="request.status == 'completed'"></div>
                                                     </div>
                                                 </td>
-                                                <td>@{{ request.nom_business_locations }}</td>
+                                                <td class="text-center">@{{ request.nom_business_locations }}</td>
                                             </tr>
 
                                             <tr v-else>
@@ -109,21 +102,17 @@
     
 $(document).ready(function() {
  var table= $('#retailersTable').DataTable({
-    	
-          fixedColumns: true,
+    	  fixedColumns: true,
           "lengthChange": false,
-          "searching": false,
+          "searching": true,
   		  "info": false,
   		  "ordering": false,
   		  "paging": false,
-    	 columnDefs: [
-                {
-                    render: function (data, type, full, meta) {
-                    	return '<span data-toggle="tooltip" data-placement="top" title="'+data+'">'+data+'</span>';
-                    },
-                    targets: -1
-                }
-             ],
+  		  
+           "language": {  
+            	search: '',
+        		"searchPlaceholder": "Search ",
+           },
              
         scrollX:        true,
         scrollCollapse: true,
@@ -144,11 +133,9 @@ $(document).ready(function() {
 
             },
             methods: {
-                openOrder(order_id){
-                    {{--window.location.href = "{{url('doorder/single-order')}}/"+order_id;--}}
-                },
+               
                 parseDateTime(date) {
-                    console.log(date);
+                    //console.log(date);
                     let dateTime = '';
                     //let parseDate = new Date();
                     let date_moment = new moment();
@@ -156,12 +143,8 @@ $(document).ready(function() {
                         //parseDate = new Date(date);
                         date_moment = new moment(date);
                     }
-                    /*dateTime += parseDate.getDate() + '/';
-                    dateTime += parseDate.getMonth() + '/';
-                    dateTime += parseDate.getFullYear() + ' ';
-                    dateTime += parseDate.getHours() + ':';
-                    dateTime += parseDate.getMinutes();*/
-                    dateTime = date_moment.format('DD-MM-YYYY HH:mm');
+                   
+                    dateTime = date_moment.format('DD MMM YY HH:mm');
                     return dateTime;
                 },
                 openRequest(request_id) {

33fcbf609805db0a81fcc2e7688fbc51455b9767
Hisham Magdy <hisham.magdy@311-solutions.com>
2021-12-08 12:58:42 +0200

    GardenHelp check if contractor is notifiable while assign a job to the contractor

--- app/Http/Controllers/garden_help/JobsController.php
+++ app/Http/Controllers/garden_help/JobsController.php
@@ -122,7 +122,9 @@ class JobsController extends Controller
                 'order_id' => $job_id
             ]);
         }
-        TwilioHelper::sendSMS('GardenHelp', $contractor->user->phone, "Hi $contractor->name, there is an job assigned to you, please open your app. " . url('contractors_app#/order-details/' . $job_id));
+        if ($contractor->is_notifiable) {
+            TwilioHelper::sendSMS('GardenHelp', $contractor->user->phone, "Hi $contractor->name, there is an job assigned to you, please open your app. " . url('contractors_app#/order-details/' . $job_id));
+        }
 
         alert()->success("The job has been successfully assigned to $contractor->name");
         return redirect()->to('garden-help/jobs_table/jobs');

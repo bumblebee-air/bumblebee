276659e6531ea900919ec8f4d4fa3b6ec84a2dd6
Hisham Magdy <hisham.magdy@311-solutions.com>
2021-12-08 12:32:10 +0200

    Gardenhelp adding orders list to side nav in contractor app

--- resources/assets/garden_help_app/js/components/partials/SidebarComponent.vue
+++ resources/assets/garden_help_app/js/components/partials/SidebarComponent.vue
@@ -7,6 +7,7 @@
 <!--            <a href="#" class="list-group-item list-group-item-action bg-light">Overview</a>-->
 <!--            <a href="#" class="list-group-item list-group-item-action bg-light">Events</a>-->
 <!--            <router-link :to="{name: 'profile'}" href="#" class="list-group-item list-group-item-action bg-light">Profile</router-link>-->
+            <router-link :to="{name: 'orders-list'}" href="#" class="list-group-item list-group-item-action bg-light">Jobs list</router-link>
             <router-link :to="{name: 'settings'}" href="#" class="list-group-item list-group-item-action bg-light">Settings</router-link>
             <a @click="logout" class="list-group-item list-group-item-action bg-light">Logout</a>
         </div>

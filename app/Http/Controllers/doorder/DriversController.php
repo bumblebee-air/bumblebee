f3be72919022d8bae956eee5f3445a9026b2ba46
Hisham Magdy <hisham.magdy@311-solutions.com>
2022-01-25 11:35:37 +0200

    DoOrder change retailer name position in on route SMS

--- app/Http/Controllers/doorder/DriversController.php
+++ app/Http/Controllers/doorder/DriversController.php
@@ -219,7 +219,7 @@ class DriversController extends Controller
                         $order->customer_phone,
                         [
                             "from" => $sender_name,
-                            "body" => "Hi $order->customer_name, DoOrder’s same day delivery service has your order and its on its way, open the link to track it and confirm the delivery afterwards. " . url('customer/order/' . $order->customer_confirmation_code) . " - $retailer_name"
+                            "body" => "Hi $order->customer_name, DoOrder’s same day delivery service has your $retailer_name order and its on its way, open the link to track it and confirm the delivery afterwards. " . url('customer/order/' . $order->customer_confirmation_code)
                         ]
                     );
                 } catch (\Exception $exception) {

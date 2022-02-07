70d93816d41374ab322212538ef7156b99162caa
Hisham Magdy <hisham.magdy@311-solutions.com>
2022-02-07 11:57:22 +0200

    DoOrder print retailers invoice without popup

--- resources/views/admin/doorder/invoice/single_invoice.blade.php
+++ resources/views/admin/doorder/invoice/single_invoice.blade.php
@@ -438,20 +438,38 @@ $( document ).ready(function() {
 				},
 				PrintElem()
 				{
-					var mywindow = window.open('', 'PRINT', 'height=400,width=600');
+					// var mywindow = window.open('', 'PRINT', 'height=400,width=600');
+					//
+					// mywindow.document.write('<html><head><title>' + document.title  + '</title>');
+					// mywindow.document.write('</head><body >');
+					// mywindow.document.write('<h1>' + document.title  + '</h1>');
+					// mywindow.document.write(document.getElementById('invoiceCard').innerHTML);
+					// mywindow.document.write('</body></html>');
+					//
+					// mywindow.document.close(); // necessary for IE >= 10
+					// mywindow.focus(); // necessary for IE >= 10*/
+					//
+					// mywindow.print();
+					// mywindow.close();
 
-					mywindow.document.write('<html><head><title>' + document.title  + '</title>');
-					mywindow.document.write('</head><body >');
-					mywindow.document.write('<h1>' + document.title  + '</h1>');
-					mywindow.document.write(document.getElementById('invoiceCard').innerHTML);
-					mywindow.document.write('</body></html>');
-
-					mywindow.document.close(); // necessary for IE >= 10
-					mywindow.focus(); // necessary for IE >= 10*/
-
-					mywindow.print();
-					mywindow.close();
-
+					var contents = document.getElementById('invoiceCard').innerHTML;
+					var frame1 = document.createElement('iframe');
+					frame1.name = "frame1";
+					frame1.style.position = "absolute";
+					frame1.style.top = "-1000000px";
+					document.body.appendChild(frame1);
+					var frameDoc = frame1.contentWindow ? frame1.contentWindow : frame1.contentDocument.document ? frame1.contentDocument.document : frame1.contentDocument;
+					frameDoc.document.open('', 'PRINT', 'height=400,width=600');
+					frameDoc.document.write(`<html><head><title>document.title</title>`);
+					frameDoc.document.write('</head><body>');
+					frameDoc.document.write(contents);
+					frameDoc.document.write('</body></html>');
+					frameDoc.document.close();
+					setTimeout(function () {
+						window.frames["frame1"].focus();
+						window.frames["frame1"].print();
+						document.body.removeChild(frame1);
+					}, 500);
 					return true;
 				}
 			}

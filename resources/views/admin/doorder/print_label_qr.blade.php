901fc1dd97e07ae6ab3b53c45180d7c478a3e913
Sara Yassen <sarayassen.118@gmail.com>
2022-03-18 01:12:11 +0200

    doorder: edit print label qr css

--- resources/views/admin/doorder/print_label_qr.blade.php
+++ resources/views/admin/doorder/print_label_qr.blade.php
@@ -14,18 +14,29 @@
 
 .td-50 {
 	width: 50%;
+	padding:15px 10px
 }
 .td-100 {
 	width: 100%;
+	padding:15px 10px
 }
 </style>
 @endsection @section('content')
-<div class="" style="width:874px; max-width: 874px">
-	<table class="table-borderless">
+<div class="" style="width:874px; max-width: 874px; padding-left:0">
+	<table class="pl-0 " style="border: 2px solid #000">
 		<tbody>
 			<!-- tr represents .row and td represents .col -->
 			<tr>
-				<td class="td-50"><div class="form-group ">
+			<td colspan="2" class="td-100 py-2" style="border-bottom: 2px solid black;">
+					<div class="my-2  py-1">
+						<img src="{{asset('images/doorder-logo.png')}}"
+							style="width: 200px; height: 140px">
+					</div>
+					 </td>			</tr>
+			<tr>
+				<td class="td-50">
+				<div class="my-2" style="margin-left:10px">
+					<div class="form-group ">
 						<label class="control-label  mb-0">Name: </label> <span
 							class="control-label  " style="display: block; font-weight: 600">
 							{{$name}} </span>
@@ -45,32 +56,26 @@
 							class="control-label" style="display: block; font-weight: 600">
 							{{$customer_phone}}</span>
 
-					</div></td>
-				<td class="td-50 pl-5">
-					<div class="text-center">
-						<div class="delivery-qrcode-reader">
+					</div></div></td>
+				<td class="td-50 ">
+						<div class="delivery-qrcode-reader" style="margin-right: 10px">
 							<img src="data:image/svg+xml;base64,{{ base64_encode($qr_str) }}" />
-						</div>
-					</div>
-					<div class="text-center  py-3">
-						<img src="{{asset('images/doorder-logo.png')}}"
-							style="width: 200px; height: 140px">
+						
 					</div></td>
 			</tr>
 			<tr>
 				<td colspan="2" class="td-100"
 					style="border-bottom: 2px solid black; border-top: 2px solid black;">
-					<label class="control-label mb-0">From: </label>
+					<div class="form-group my-2"  style="margin-left:10px"><label class="control-label mb-0">From: </label>
 					<span class="control-label"
-						  style="display: block; font-weight: 600">{{$from_name}}</span>
-					<span class="control-label"
-						  style="display: block; font-weight: 600">{!! $from_address !!}</span>
+						  style="display: block; font-weight: 600">{{$from_name}}</span> </div>
+					
 				</td>
 			</tr>
 			@if(isset($package_no) && isset($package_total))
 			<tr>
 				<td colspan="2" class="td-100 text-center">
-					<h4>Package {{$package_no}} of {{$package_total}}</h4>
+					<h4 class="my-2">Package {{$package_no}} of {{$package_total}}</h4>
 				</td>
 			</tr>
 			@endif
